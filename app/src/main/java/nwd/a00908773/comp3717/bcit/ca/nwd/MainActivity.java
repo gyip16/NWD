5195456d578f8704a65930e15dcaac09491e6adb
Gabriel Yip <gyip16@my.bcit.ca>
2017-03-09 09:49:49 -0800

    Changed UI on main activity. added spinners and hooked up onItemSelectedListener

--- app/src/main/java/nwd/a00908773/comp3717/bcit/ca/nwd/MainActivity.java
+++ app/src/main/java/nwd/a00908773/comp3717/bcit/ca/nwd/MainActivity.java
@@ -5,15 +5,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.graph_type);
+        Spinner spinner2 = (Spinner) findViewById(R.id.opendata);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.graph_type, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.opendata, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
     }
 
     public void download(View v)
@@ -29,4 +49,26 @@ public class MainActivity extends AppCompatActivity {
         Intent settingsIntent = new Intent(getApplicationContext(),SettingsActivity.class);
         startActivity(settingsIntent);
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        TextView v;
+        switch(parent.getId()){
+            case R.id.graph_type:
+                v = (TextView) findViewById(R.id.graphtext);
+                v.setText(parent.getItemAtPosition(pos).toString());
+                break;
+            case R.id.opendata:
+                v = (TextView) findViewById(R.id.openDataset);
+                v.setText(parent.getItemAtPosition(pos).toString());
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
